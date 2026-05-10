1c653a9c3c1c413bd0977490c52d1893bee5b9f8
Y213 <y213333yfvh6tg@outlook.com>
2026-05-10 17:55:10 +0800

    Enhance token handling and whitespace management
    Added functions for reading various token types and handling whitespace in the tokenization process.

--- myToken.h
+++ myToken.h
@@ -2,8 +2,35 @@
 #define MYTOKEN_H
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#define SAFE_PUSH_NEXT(tok,ch) do{\
+	int ret=pushToken(tok,ch);\
+	if(ret==1){\
+		tok->T=TOKEN_ERR;\
+		return 1;\
+	}\
+	if(ret==2){\
+		tok->T=TOKEN_ERR;\
+		return 2;\
+	}\
+	nextChar(rfp,pch);\
+}while(0)
+#define SAFE_PUSH(tok,ch) do{\
+	int ret=pushToken(tok,ch);\
+	if(ret==1){\
+		tok->T=TOKEN_ERR;\
+		return 1;\
+	}\
+	if(ret==2){\
+		tok->T=TOKEN_ERR;\
+		return 2;\
+	}\
+}while(0)
+//并非SAFE
 typedef enum{
 	TOKEN_VOID,
+	TOKEN_ERR,
 	
 	TOKEN_EOF,
 	TOKEN_WORD,
@@ -51,6 +78,7 @@ token* newToken(){
 	tok->size=10;
 	tok->T=TOKEN_VOID;
 	tok->text=s;
+	tok->text[0]='\0';
 	return tok;
 }
 int pushToken(token* tok,char ch){
@@ -76,5 +104,383 @@ void freeToken(token* tok){
 	if(tok==NULL)return;
 	free(tok->text);
 	free(tok);
+}
+void nextChar(FILE* rfp,int* pch){
+	pch[0]=fgetc(rfp);
+}
+void skipWhiteSpace(FILE* rfp,int* pch){
+	while(pch[0]!=EOF){
+		if(pch[0]==' '||pch[0]=='\t'||pch[0]=='\r'||pch[0]=='\n'){
+			nextChar(rfp,pch);
+		}else break;
+	}
+}
+int readIdentifier(FILE* rfp,int* pch,token* tok){
+	while(isalnum(pch[0])||pch[0]=='_'){
+		SAFE_PUSH_NEXT(tok,pch[0]);
+	}
+	tok->T=TOKEN_WORD;
+	return 0;
+}
+int readNumber(FILE* rfp,int* pch,token* tok){
+	while(
+		isdigit(pch[0])||pch[0]=='.'
+		||pch[0]=='X'||pch[0]=='x'
+		||pch[0]=='E'||pch[0]=='e'
+		||((pch[0]=='+'||pch[0]=='-')
+		&&(tok->len>0&&(tok->text[tok->len-1]=='e'||tok->text[tok->len-1]=='E')))
+	){
+		SAFE_PUSH_NEXT(tok,pch[0]);
+	}
+	tok->T=TOKEN_NUMBER;
+	return 0;
+}
+int readString(FILE* rfp,int* pch,token* tok){
+	SAFE_PUSH_NEXT(tok,'"');
+	while(pch[0]!='"'&&pch[0]!=EOF){
+		if(pch[0]=='\\'){
+			SAFE_PUSH_NEXT(tok,pch[0]);
+		}
+		SAFE_PUSH_NEXT(tok,pch[0]);
+	}
+	if(pch[0]=='"'){
+		SAFE_PUSH_NEXT(tok,pch[0]);
+	}
+	tok->T=TOKEN_STRING;
+	return 0;
+}
+int readChar(FILE* rfp,int* pch,token* tok){
+	SAFE_PUSH_NEXT(tok,'\'');
+	while(pch[0]!='\''&&pch[0]!=EOF){
+		if(pch[0]=='\\'){
+			SAFE_PUSH_NEXT(tok,pch[0]);
+		}
+		SAFE_PUSH_NEXT(tok,pch[0]);
+	}
+	if(pch[0]=='\''){
+		SAFE_PUSH_NEXT(tok,pch[0]);
+	}
+	tok->T=TOKEN_CHAR;
+	return 0;
+}
+int readLineComment(FILE* rfp,int* pch,token* tok){
+	SAFE_PUSH(tok,'/');
+	SAFE_PUSH(tok,'/');
+	nextChar(rfp,pch);
+	while(pch[0]!='\n'&&pch[0]!=EOF){
+		SAFE_PUSH_NEXT(tok,pch[0]);
+	}
+	tok->T=TOKEN_COMMENT;
+	return 0;
+}
+int readBlockComment(FILE* rfp,int* pch,token* tok){
+	SAFE_PUSH(tok,'/');
+	SAFE_PUSH(tok,'*');
+	nextChar(rfp,pch);
+	while(pch[0]!='\n'&&pch[0]!=EOF){
+		if(pch[0]=='*'){
+			SAFE_PUSH_NEXT(tok,pch[0]);
+			if(pch[0]=='/'){
+				SAFE_PUSH_NEXT(tok,pch[0]);
+			}
+		}else{
+			SAFE_PUSH_NEXT(tok,pch[0]);
+		}
+	}
+	tok->T=TOKEN_COMMENT;
+	return 0;
+}
+int readPreprocessor(FILE* rfp,int* pch,token* tok){
+	while(pch[0]!=EOF&&pch[0]!='\n'){
+		if(pch[0]=='\\'){
+			SAFE_PUSH_NEXT(tok,pch[0]);
+			if(pch[0]=='\n'){
+				SAFE_PUSH_NEXT(tok,pch[0]);
+			}
+		}else{
+			SAFE_PUSH_NEXT(tok,pch[0]);
+		}
+	}
+	tok->T=TOKEN_PREPROC;
+	return 0;
+}
+int readOperator(FILE* rfp,int* pch,token* tok){
+	char first=pch[0];
+	SAFE_PUSH_NEXT(tok,pch[0]);
+	if ((first=='='&&pch[0]== '=')||
+	        (first=='!'&&pch[0]=='=')||
+	        (first=='<'&&pch[0]=='=')||
+	        (first=='>'&&pch[0]=='=')||
+	        (first=='&'&&pch[0]=='&')||
+	        (first=='|'&&pch[0]=='|')||
+	        (first=='+'&&pch[0]=='+')||
+	        (first=='-'&&pch[0]=='-')||
+	        (first=='-'&&pch[0]=='>')||
+	        (first=='+'&&pch[0]=='=')||
+	        (first=='-'&&pch[0]=='=')||
+	        (first=='*'&&pch[0]=='=')||
+	        (first=='/'&&pch[0]=='=')||
+	        (first=='%'&&pch[0]=='=')||
+	        (first=='&'&&pch[0]=='=')||
+	        (first=='|'&&pch[0]=='=')||
+	        (first=='^'&&pch[0]=='=')||
+	        (first=='<'&&pch[0]=='<')||
+	        (first=='>'&&pch[0]=='>')){
+	        SAFE_PUSH_NEXT(tok,pch[0]);
+	}
+	if(strcmp(tok->text,".")==0||strcmp(tok->text,"->")==0){
+		tok->T=TOKEN_DOT;
+	}
+	else{
+		tok->T=TOKEN_OPERATOR;
+	}
+	return 0;
+}
+int getToken(FILE* rfp,int* pch,token* tok){
+	skipWhiteSpace(rfp,pch);
+	if(pch[0]==EOF){
+		tok->T=TOKEN_EOF;
+		return 0;
+	}
+	if(pch[0]=='#'){
+		int ret=readPreprocessor(rfp,pch,tok);
+		return ret;
+	}
+	if(isalpha(pch[0])||pch[0]=='_'){
+		int ret=readIdentifier(rfp,pch,tok);
+		return ret;
+	}
+	if(isdigit(pch[0])){
+		int ret=readNumber(rfp,pch,tok);
+		return ret;
+	}
+	if(pch[0]=='"'){
+		int ret=readString(rfp,pch,tok);
+		return ret;
+	}
+	if(pch[0]=='\''){
+		int ret=readChar(rfp,pch,tok);
+		return ret;
+	}
+	if(pch[0]=='/'){
+		nextChar(rfp,pch);
+		if(pch[0]=='/'){
+			int ret=readLineComment(rfp,pch,tok);
+			return ret;
+		}
+		if(pch[0]=='*'){
+			int ret=readBlockComment(rfp,pch,tok);
+			return ret;
+		}
+		ungetc(pch[0],rfp);
+		pch[0]='/';
+		int ret=readOperator(rfp,pch,tok);
+		return ret;
+	}
+	int ret;
+	switch(pch[0]){
+		case '{':
+			tok->T=TOKEN_LBRACE;
+			ret=pushToken(tok,'{');
+			nextChar(rfp,pch);
+			return ret;
+		case '}':
+			tok->T=TOKEN_RBRACE;
+			ret=pushToken(tok,'}');
+			nextChar(rfp,pch);
+			return ret;
+		case '(':
+			tok->T=TOKEN_LPAREN;
+			ret=pushToken(tok,'(');
+			nextChar(rfp,pch);
+			return ret;
+		case ')':
+			tok->T=TOKEN_RPAREN;
+			ret=pushToken(tok,')');
+			nextChar(rfp,pch);
+			return ret;
+		case '[':
+			tok->T=TOKEN_LBRACKET;
+			ret=pushToken(tok,'[');
+			nextChar(rfp,pch);
+			return ret;
+		case ']':
+			tok->T=TOKEN_RBRACKET;
+			ret=pushToken(tok,']');
+			nextChar(rfp,pch);
+			return ret;
+		case ';':
+			tok->T=TOKEN_SEMICOLON;
+			ret=pushToken(tok,';');
+			nextChar(rfp,pch);
+			return ret;
+		case ',':
+			tok->T=TOKEN_COMMA;
+			ret=pushToken(tok,',');
+			nextChar(rfp,pch);
+			return ret;
+		case ':':
+			tok->T=TOKEN_COLON;
+			ret=pushToken(tok,':');
+			nextChar(rfp,pch);
+			return ret;
+		case '.':
+			nextChar(rfp,pch);
+			if(isdigit(pch[0])){
+				ungetc(pch[0],rfp);
+				pch[0]='.';
+				int ret=readNumber(rfp,pch,tok);
+				return ret;
+			}
+			ungetc(pch[0],rfp);
+			pch[0]='.';
+			tok->T=TOKEN_DOT;
+			ret=pushToken(tok,'.');
+			nextChar(rfp,pch);
+			return ret;
+		default:
+			if(ispunct(pch[0])){
+				ret=readOperator(rfp,pch,tok);
+				return ret;
+			}else{
+				tok->T=TOKEN_ERR;
+				nextChar(rfp,pch);
+				return 5;
+			}
+	}
+	return 0;
+}
+int needSpaceBefore(token* prev,token* cur){
+    if(prev==NULL||cur==NULL)return 0;
+    if(cur->T==TOKEN_COMMENT)return 0;
+    if(cur->T==TOKEN_PREPROC)return 0;
+    if(prev->T==TOKEN_COMMENT)return 0;
+    if(prev->T==TOKEN_PREPROC)return 0;
+    if(prev->T==TOKEN_COMMA)return 1;
+    if(cur->T==TOKEN_LPAREN){
+        if(prev->T==TOKEN_WORD&&
+            (strcmp(prev->text,"if")==0||
+             strcmp(prev->text,"while")==0||
+             strcmp(prev->text,"for")==0||
+             strcmp(prev->text,"switch")==0||
+             strcmp(prev->text,"return")==0))
+            return 1;
+        return 0;
+    }
+    if(prev->T==TOKEN_RPAREN&&
+        (cur->T==TOKEN_LBRACE||cur->T==TOKEN_WORD))
+        return 1;
+    if(prev->T==TOKEN_LPAREN||prev->T==TOKEN_LBRACKET)
+	    return 0;
+	if(cur->T==TOKEN_RPAREN||cur->T==TOKEN_RBRACKET)
+	    return 0;
+    if(cur->T==TOKEN_OPERATOR||prev->T==TOKEN_OPERATOR)
+        return 1;
+    if(prev->T==TOKEN_WORD&&cur->T==TOKEN_WORD)
+	    return 1;
+    return 0;
+}
+int formatting(char* rFileName){
+	FILE* rfp=fopen(rFileName,"rb");
+	if(rfp==NULL){
+		fprintf(stderr,"打开文件失败\n");
+		return 3;
+	}
+	char wFileName[512];
+	strcpy(wFileName,rFileName);
+	strcat(wFileName,"_f");
+	FILE* wfp=fopen(wFileName,"wb");
+	if(wfp==NULL){
+		fprintf(stderr,"打开输出文件失败\n");
+		fclose(rfp);
+		return 4;
+	}
+	int currentChar='\0';
+	int indentLv=0;
+	int beginOfLine=1;
+	token* prev=NULL;
+	token* cur=newToken();
+	if(cur==NULL)return 1;
+	int* pch=&currentChar;
+	getToken(rfp,pch,cur);
+	while(cur->T!=TOKEN_EOF){
+		if(cur->T==TOKEN_PREPROC){
+			if(!beginOfLine)fputc('\n',wfp);
+		    	fputs(cur->text,wfp);
+		    if(cur->text[cur->len-1]!='\n')
+		        fputc('\n',wfp);
+		    beginOfLine=1;
+		    freeToken(prev);
+			prev=cur;
+		    cur=newToken();
+			getToken(rfp,pch,cur);
+		    continue;
+		}
+		if(cur->T==TOKEN_COMMENT){
+			if(beginOfLine){
+				for(int i=0;i<indentLv*4;i++)
+					fputc(' ',wfp);
+				beginOfLine=0;
+			}
+		    if(!beginOfLine&&needSpaceBefore(prev,cur))
+		        fputc(' ',wfp);
+		    fputs(cur->text,wfp);
+		    if(cur->len>0&&cur->text[cur->len-1]=='\n')
+		        beginOfLine=1;
+		    else{
+		        beginOfLine=0;
+		        freeToken(prev);
+				prev=cur;
+			}
+		    cur=newToken();
+			getToken(rfp,pch,cur);
+		    continue;
+		}
+		if(!beginOfLine&&needSpaceBefore(prev,cur))
+		    fputc(' ', wfp);
+		if(cur->T==TOKEN_LBRACE){
+		    if(!beginOfLine)fputc('\n',wfp);
+		    for(int i=0;i<indentLv*4;i++)
+				fputc(' ',wfp);
+		    fputs("{",wfp);
+		    indentLv++;
+		    fputc('\n',wfp);
+		    beginOfLine=1;
+		}
+		else if(cur->T==TOKEN_RBRACE){
+		    indentLv--;
+		    if(!beginOfLine)fputc('\n',wfp);
+		    for(int i=0;i<indentLv*4;i++)
+				fputc(' ', wfp);
+		    fputs("}",wfp);
+		    fputc('\n',wfp);
+		    beginOfLine=1;
+		}
+		else if(cur->T == TOKEN_SEMICOLON){
+		    fputs(";",wfp);
+		    fputc('\n',wfp);
+		    beginOfLine=1;
+		}
+		else{
+		    if(beginOfLine){
+		        for(int i=0;i<indentLv*4;i++)
+					fputc(' ',wfp);
+		        beginOfLine=0;
+		    }
+		    fputs(cur->text,wfp);
+		}
+		freeToken(prev);
+		prev=cur;
+		cur=newToken();
+		getToken(rfp,pch,cur);
+	}
+	if(!beginOfLine){
+		fputc('\n',wfp);
+	}
+	freeToken(prev);
+	freeToken(cur);
+	fclose(wfp);
+	fclose(rfp);
+	return 0;
 }
 #endif
